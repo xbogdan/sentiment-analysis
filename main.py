d7b0450f5fce02b9bf5fa560ef3d47efd0d1dcec
Bogdan Boamfa <bogdanboamfa@link-busters.com>
2017-05-19 17:57:26 +0300

    Fixed Naive Bayes.

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
+import os
+from random import shuffle
+
 from naive_bayes import NaiveBayes
 
 
 def import_lexicon(path):
     keys = set()
-    with open(path, encoding='utf-8') as file:
+    with open(path, encoding='utf-8', errors='ignore') as file:
         for line in file:
             if line.startswith(';'):
                 continue
@@ -14,11 +17,59 @@ def import_lexicon(path):
     return keys
 
 
-def main():
-    pos_keys = import_lexicon('lexicons/positive-words.txt')
-    # neg_keys = import_lexicon('lexicons/negative-words.txt')
+def read_from_dir(directory, file_type='.txt', limit=800):
+    context_list = []
 
-    NaiveBayes().train([], ['positive', 'positive', 'negative', 'negative', 'negative'])
+    ll = os.listdir(directory)
+    shuffle(ll)
+
+    file_list = ll[:limit] if limit else os.listdir(directory)
+    for filename in file_list:
+        if file_type and filename.endswith(file_type) or not file_type:
+            with open(f'{directory}/{filename}', 'r', encoding='utf-8', errors='ignore') as file:
+                context_list.append(file.read())
+
+    test_list = []
+    test = ll[limit:] if limit else os.listdir(directory)
+    for filename in test:
+        if file_type and filename.endswith(file_type) or not file_type:
+            with open(f'{directory}/{filename}', 'r', encoding='utf-8', errors='ignore') as file:
+                test_list.append(file.read())
+
+    return context_list, test_list
+
+
+def main():
+    # pos_keys = import_lexicon('training_sets/lexicons/positive-words.txt')
+    # neg_keys = import_lexicon('training_sets/lexicons/negative-words.txt')
+
+    pos_train_set, pos_test_set = read_from_dir('training_sets/reviews/pos1')
+    neg_train_set, neg_test_set = read_from_dir('training_sets/reviews/neg1')
+    print(len(pos_train_set), len(pos_test_set))
+    print(len(neg_train_set), len(neg_test_set))
+
+    nb = NaiveBayes()
+    nb.train(vocabularies=pos_train_set+neg_train_set,
+             classes=['positive' for _ in range(0, len(pos_train_set))] +
+                     ['negative' for _ in range(0, len(neg_train_set))])
+
+    pos_returns = {'positive': 0, 'negative': 0}
+    for value in pos_test_set:
+        # print(nb.identify(pos_value))
+        if nb.identify(value) == 'positive':
+            pos_returns['positive'] += 1
+        else:
+            pos_returns['negative'] += 1
+    print('positve', pos_returns['positive'] * 100/(pos_returns['positive']+pos_returns['negative']), '%')
+
+    pos_returns = {'positive': 0, 'negative': 0}
+    for value in neg_test_set:
+        # print(nb.identify(pos_value))
+        if nb.identify(value) == 'negative':
+            pos_returns['negative'] += 1
+        else:
+            pos_returns['positive'] += 1
+    print('negative', pos_returns['negative'] * 100 / (pos_returns['positive'] + pos_returns['negative']), '%')
 
 
 if __name__ == '__main__':
--- naive_bayes.py
+++ naive_bayes.py
@@ -2,21 +2,25 @@ from collections import Counter
 from io import BytesIO
 from tokenize import tokenize
 
+from decimal import Decimal
+
 
 class NaiveBayes:
 
     def __init__(self):
+        self.vocabulary_dict = {}
         self.class_priors = None
         self.classes = None
 
         self.nb_dict = {}
+        self.mega_doc = {}
 
     @staticmethod
-    def calculate_occurences(items):
+    def calculate_probabilities(items):
         nr_items = len(items)
         occurences = dict(Counter(items))
         for key in occurences.keys():
-            occurences[key] = occurences[key] / float(nr_items)
+            occurences[key] = occurences[key] / Decimal(nr_items)
 
         return occurences
 
@@ -45,12 +49,13 @@ class NaiveBayes:
         return text
 
     def calculate_priors(self, classes):
-        return self.calculate_occurences(classes)
+        return self.calculate_probabilities(classes)
 
     @staticmethod
-    def calculate_likelyhood(items, laplace=1):
+    def calculate_likelyhood(items, vocabulary_size, laplace=1):
         """
         Calculates the likelyhood of earch word in a vocabulary
+        :param vocabulary_size: 
         :param items: list of words
         :param laplace: Laplace smoothing
         :return: dict with the likelyhood value for each word
@@ -58,10 +63,11 @@ class NaiveBayes:
 
         nr_items = len(items)
         occurences = dict(Counter(items))
-        sum_of_counts = sum(list(occurences.values()))
 
         for key in occurences.keys():
-            occurences[key] = (occurences[key] + laplace) / float(sum_of_counts + nr_items)
+            # print(key, ' - ', occurences[key]+laplace, nr_items+ vocabulary_size)
+            occurences[key] = (occurences[key] + laplace) / Decimal(vocabulary_size + nr_items)
+            # print(key, occurences[key])
 
         return occurences
 
@@ -69,19 +75,34 @@ class NaiveBayes:
     def tokenize(text, to_ignore=None):
         """
         Tokenize a text based on specific rules
-        It depends on the type of text and the source.
+        It depends on the type of text and the source. 
         Example: html, twitter posts, text from a book, etc.
         :param text: text 
         :param to_ignore: a list of tokens to ignore
         :return: a list of words
         """
-
-        g = tokenize(BytesIO(text.encode('utf-8')).readline)
-        if to_ignore:
-            tokenized_text = [tokval for toknum, tokval, _, _, _ in g if tokval not in to_ignore]
-        else:
-            tokenized_text = [tokval for toknum, tokval, _, _, _ in g if tokval]
-
+        # byte_obj = BytesIO(text.strip().encode('utf-8'))
+        # g = tokenize(byte_obj.readline)
+        #
+        # import ipdb; ipdb.set_trace()
+        # if to_ignore:
+        #     tokenized_text = [tokval for toknum, tokval, _, _, _ in g if tokval not in to_ignore]
+        # else:
+        #     tokenized_text = [tokval for toknum, tokval, _, _, _ in g if tokval]
+        tokenized_text = text.strip()
+        tokenized_text = tokenized_text.replace(',', ' ')
+        tokenized_text = tokenized_text.replace('.', ' ')
+        tokenized_text = tokenized_text.replace(';', ' ')
+        tokenized_text = tokenized_text.replace('"', ' ')
+        tokenized_text = tokenized_text.replace('\'', ' ')
+        tokenized_text = tokenized_text.replace(':', ' ')
+        tokenized_text = tokenized_text.replace('{', ' ')
+        tokenized_text = tokenized_text.replace('}', ' ')
+        tokenized_text = tokenized_text.replace(')', ' ')
+        tokenized_text = tokenized_text.replace('(', ' ')
+        tokenized_text = tokenized_text.replace('/', ' ')
+        tokenized_text = tokenized_text.replace('?', ' ')
+        tokenized_text = tokenized_text.split()
         return tokenized_text
 
     def identify(self, text):
@@ -91,12 +112,13 @@ class NaiveBayes:
         :return: class name
         """
 
-        tokenized_text = self.tokenize(text)
+        tokenized_text = self.tokenize(text.lower())
         prior = {}
         for label in self.classes:
             prior[label] = self.class_priors[label]
             for word in tokenized_text:
-                prior[label] *= self.nb_dict[label][word]
+                if word in self.vocabulary_dict:
+                    prior[label] *= Decimal(self.vocabulary_dict[word][label])
 
         # TODO handle 50-50 cases
         max_value = 0
@@ -109,29 +131,62 @@ class NaiveBayes:
         return max_class
 
     def train(self, vocabularies, classes):
-        """"
-        When the goal is classifying text, it is better to give the input X in the form of a list of lists containing words.
+        """
+        When the goal is classifying text, it is better to give the input vocabularies 
+        in the form of a list of lists containing words.
         X = [
             ['this', 'is', 'a', ...],
             ['i', 'love', 'food', ...],
             ...
         ]
 
-        Y = ['positive', 'positive', 'negative', ...]
-        so Y[0] is the class for X[0]
+        classes = ['positive', 'positive', 'negative', ...]
+        so classes[0] is the class for vocabularies[0]
         """
 
+        # split in words each text
+        for i, v in enumerate(vocabularies):
+            vocabularies[i] = self.tokenize(v.lower())
+
+        # class probability
         self.class_priors = self.calculate_priors(classes)
 
         self.classes = set(classes)
+
+        # creates mega documents per class
         for label in self.classes:
-            self.nb_dict[label] = []
+            self.mega_doc[label] = []
 
         # combine all examples into one for each class
         # creating a mega document for each class
+        vocabulary_size = 0
+
         for i in range(0, len(classes)):
-            self.nb_dict[classes[i]].extend(vocabularies[i])
+            self.mega_doc[classes[i]].extend(vocabularies[i])
+
+        vocabulary = []
+        for label in self.classes:
+            vocabulary.extend(self.mega_doc[label])
+
+        vocabulary = set(vocabulary)
+        vocabulary_size += len(vocabulary)
+        # print('Vocabulary size', vocabulary_size)
 
         # transform the list with all occurences to a dict with relative occurences
         for label in self.classes:
-            self.nb_dict[label] = self.calculate_likelyhood(self.nb_dict[label])
+            self.nb_dict[label] = dict(Counter(self.mega_doc[label]))
+
+        for word in vocabulary:
+            self.vocabulary_dict[word] = {}
+            for label in self.classes:
+                self.vocabulary_dict[word][label] = None
+
+        for label in self.classes:
+            for word in vocabulary:
+                word_occ = self.nb_dict[label][word] if word in self.nb_dict[label] else 0
+                # print(f'{word}|{label} = ({word_occ} + 1) / ({len(self.mega_doc[label])} + {vocabulary_size})')
+                # if word == 'is':
+                #     import ipdb; ipdb.set_trace()
+                val = Decimal((word_occ + 1) / Decimal(vocabulary_size + len(self.mega_doc[label])))
+                self.vocabulary_dict[word][label] = val
+
